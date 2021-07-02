e85b4b178987068a242ff6c48768331232540234
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-07-02 23:13:41 +0900

    test - 지하철역 기준 그래프탐색 테스트 코드

--- src/test/kotlin/just/hazard/kotlinjpa/repositories/StationRepositoryTest.kt
+++ src/test/kotlin/just/hazard/kotlinjpa/repositories/StationRepositoryTest.kt
@@ -1,8 +1,13 @@
 package just.hazard.kotlinjpa.repositories
 
+import just.hazard.kotlinjpa.domain.Distance
+import just.hazard.kotlinjpa.domain.Line
+import just.hazard.kotlinjpa.domain.LineStation
 import just.hazard.kotlinjpa.domain.Station
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert
 import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
@@ -69,11 +74,35 @@ class StationRepositoryTest {
         }.isInstanceOf(NoSuchElementException::class.java)
     }
 
+    @Test
+    fun 지하철역_조회시_그래프탐색() {
+        라인_지하철역_등록()
+        val actual = stationRepository.findByName("고속터미널역")
+        지하철역_데이터_체크(actual,"고속터미널역")
+        actual.stations.forEach {
+            MatcherAssert.assertThat(it.line.name,
+                CoreMatchers.either(CoreMatchers.containsString("7호선")).or(CoreMatchers.containsString("2호선")).or(CoreMatchers.containsString("3호선")))
+        }
+    }
+
+    private fun 라인_지하철역_등록() {
+        val station_expressBusTerminal = Station("고속터미널역")
+        val station_banpo = Station("반포역")
+
+        station_expressBusTerminal.updateLineStation(LineStation(Line("연두색","7호선"),station_expressBusTerminal,station_banpo,
+            Distance(10)))
+        station_expressBusTerminal.updateLineStation(LineStation(Line("초록색","2호선"),station_expressBusTerminal,station_banpo,
+            Distance(15)))
+        station_expressBusTerminal.updateLineStation(LineStation(Line("주황색","3호선"),station_expressBusTerminal,station_banpo,
+            Distance(20)))
+        stationRepository.save(station_expressBusTerminal)
+    }
+
     private fun 지하철역_조회() = stationRepository.findById(1L).get()
 
     private fun 지하철역_데이터_체크(actual: Station, name: String) {
         assertAll(
-            { assertThat(station.name).isEqualTo(name) },
+            { assertThat(actual.name).isEqualTo(name) },
             { assertThat(actual.id).isNotNull() },
             { assertThat(actual.createdDate).isNotNull() },
             { assertThat(actual.modifiedDate).isNotNull() },
