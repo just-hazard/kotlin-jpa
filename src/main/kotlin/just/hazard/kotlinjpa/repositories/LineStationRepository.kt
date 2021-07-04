a319902d1c56fb5f6e0fca0b986cc9b1063cc849
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-07-04 16:07:19 +0900

    test - lineStation 유니크키 및 그래프탐색 테스트

--- src/main/kotlin/just/hazard/kotlinjpa/repositories/LineStationRepository.kt
+++ src/main/kotlin/just/hazard/kotlinjpa/repositories/LineStationRepository.kt
@@ -4,4 +4,5 @@ import just.hazard.kotlinjpa.domain.LineStation
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface LineStationRepository: JpaRepository<LineStation, Long> {
+    fun findByLineId(lindId: Long): MutableList<LineStation>
 }
--- src/test/kotlin/just/hazard/kotlinjpa/repositories/LineStationRepositoryTest.kt
+++ src/test/kotlin/just/hazard/kotlinjpa/repositories/LineStationRepositoryTest.kt
@@ -1,9 +1,17 @@
 package just.hazard.kotlinjpa.repositories
 
+import just.hazard.kotlinjpa.domain.Distance
+import just.hazard.kotlinjpa.domain.Line
+import just.hazard.kotlinjpa.domain.LineStation
+import just.hazard.kotlinjpa.domain.Station
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.dao.DataIntegrityViolationException
 import javax.persistence.EntityManager
 
 @DataJpaTest
@@ -21,9 +29,14 @@ class LineStationRepositoryTest {
     @Autowired
     private lateinit var entityManager: EntityManager
 
+    val 칠호선 = Line("연두색","7호선")
+    val 수락산역 = Station("수락산역")
+    val 건대입구역 = Station("건대입구역")
+    val 청담역 = Station("청담역")
+
     @BeforeEach
     fun setUp() {
-
+        lineStationRepository.save(LineStation(칠호선,수락산역,건대입구역, Distance(10)))
     }
 
     @AfterEach
@@ -32,5 +45,26 @@ class LineStationRepositoryTest {
         entityManager
             .createNativeQuery("alter table line_station alter column id restart 1")
             .executeUpdate()
+    }
+
+    @Test
+    fun 유니크키_중복체크_라인_이전역_현재역() {
+        assertThrows<DataIntegrityViolationException> {
+            lineStationRepository.save(LineStation(lineRepository.findByName("7호선"),
+                stationRepository.findByName("수락산역"),
+                stationRepository.findByName("건대입구역"), Distance(15)))
+        }
+        entityManager.clear()
+    }
+
+    @Test
+    fun 그래프_탐색() {
+        // given
+        lineStationRepository.save(LineStation(칠호선,건대입구역,청담역, Distance(15)))
+        // when
+        val lineStations = lineStationRepository.findByLineId(1L)
+        // then
+        assertThat(lineStations[0].station.name).isEqualTo("건대입구역")
+        assertThat(lineStations[1].station.name).isEqualTo("청담역")
     }
 }
