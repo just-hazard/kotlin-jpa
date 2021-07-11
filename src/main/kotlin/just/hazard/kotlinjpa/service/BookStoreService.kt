df3baf3e7e5d3e8006d48533bd8627bc1f4c8db5
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-07-11 22:52:06 +0900

    refactor - java 8 Map.merge() 지원 안해서 리팩터링 중

--- src/main/kotlin/just/hazard/kotlinjpa/service/BookStoreService.kt
+++ src/main/kotlin/just/hazard/kotlinjpa/service/BookStoreService.kt
@@ -4,6 +4,8 @@ import just.hazard.kotlinjpa.domain.bookstore.Book
 import just.hazard.kotlinjpa.repositories.BookRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.util.*
+import java.util.stream.Collectors.toMap
 
 @Service
 class BookStoreService(private val bookRepository: BookRepository) {
@@ -11,18 +13,13 @@ class BookStoreService(private val bookRepository: BookRepository) {
     @Transactional(readOnly = true)
     fun findBookNameSorting() : Map<String,List<Book>> {
         val bookMap: MutableMap<String,List<Book>> = HashMap()
-
         val books = bookRepository.findAll()
         // 책 조회 후 빠른 탐색을 위해 책 이름을 기준으로 List를 Map에 담기
-        books.stream()
+        books
             .map {
-                bookMap.merge(
-                    it.bookName,
-                    mutableListOf(it),
-
-                )
+                bookMap.put(it.bookName, listOf(it))
             }
-
-        return mutableMapOf()
+        return bookMap
     }
 }
+
