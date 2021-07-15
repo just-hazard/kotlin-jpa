10b5d50311c9a4f8c9f877067e66a07d6b25cc45
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-07-15 22:33:24 +0900

    refactor,test - merge 사용방법 없을까

--- src/main/kotlin/just/hazard/kotlinjpa/service/BookStoreService.kt
+++ src/main/kotlin/just/hazard/kotlinjpa/service/BookStoreService.kt
@@ -12,13 +12,23 @@ class BookStoreService(private val bookRepository: BookRepository) {
     fun findBookNameSorting() : Map<String,MutableList<Book>> {
         val bookMap: MutableMap<String,MutableList<Book>> = HashMap()
         val books = bookRepository.findAll()
-        // 책 조회 후 빠른 탐색을 위해 책 이름을 기준으로 List를 Map에 담기
-        books.filter {
-            !bookMap.containsKey(it.bookName)
-        }.map {
-            bookMap.put(it.bookName, mutableListOf(it))
+        books.forEach {
+            bookMap.merge(it.bookName, mutableListOf(it), { _, _ -> )
         }
+        // 책 조회 후 빠른 탐색을 위해 책 이름을 기준으로 List를 Map에 담기
+//        val unionList = (mapA.asSequence() + mapB.asSequence())
+//            .distinct()
+//            .groupBy({ it.key }, { it.value })
+//            .mapValues { (_, values) -> values.joinToString(",") }
         return bookMap
+    }
+
+    fun <K, V> Map<K, V>.mergeReduce(other: Map<K, V>, reduce: (V, V) -> V): Map<K, V> {
+        val result = LinkedHashMap<K, V>(this)
+        for ((key, value) in other) {
+            result.merge(key, value, reduce)
+        }
+        return result
     }
 }
 
